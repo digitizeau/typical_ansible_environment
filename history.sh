f82b81b6755b5bb3678c4dac78b3947b3cc09a12
Luke Ooyendyk <40414953+ooyendyk@users.noreply.github.com>
2023-01-18 12:25:34 +1100

    Update history.sh

--- history.sh
+++ history.sh
@@ -1,4 +1,7 @@
-  ansible-navigator inventory --graph
+[rhel@control ansible-files]$ history
+    1  cd ansible-files
+    2  ansible-navigator inventory --list
+    3  ansible-navigator inventory --graph
     4  cd ansible-files
     5  ansible-navigator run apache.yml
     6  ansible node1 -m ansible.builtin.shell -a "rpm -qi httpd"
@@ -12,3 +15,9 @@
    14  ansible-navigator run apache.yml
    15  curl http://node1
    16  history
+   17  cd ansible-files
+   18  ansible-navigator run apache.yml
+   19  ansible web -m ansible.builtin.service_facts | grep httpd.service -A4
+   20  cd ansible-files
+   21  ansible-navigator run apache.yml
+   22  history
